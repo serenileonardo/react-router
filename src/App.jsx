be4cd452453affc66f87a61e717808f7ff639aeb
serenileonardo <serenileonardo.web@gmail.com>
2026-01-28 15:22:55 +0100

    fix: scaffolding

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Products from "./pages/Products"
+import "./index.css"
 
 function App() {
   return (
