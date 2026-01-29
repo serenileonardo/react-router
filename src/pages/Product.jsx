e46fa1c8a4e88a2055a23694858082d92a0f5e3a
serenileonardo <serenileonardo.web@gmail.com>
2026-01-29 16:16:23 +0100

    fix: fix id

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -19,7 +19,7 @@ export default function Product() {
     return (
         <div>
             <h3>{product.title}</h3>
-            <img src={product.image} width="150px" />
+            <img src={product.image} />
             <p>{product.description}</p>
             <p>{product.price} </p>
         </div>
